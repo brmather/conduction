BUG!! Fixed adjoint of the covariance matrix

inversion/nd_inverse_conduction.py
@@ -564,22 +564,17 @@ class InversionND(object):
 
         misfit = np.array(x - x0)
         lhs, rhs = cov.createVecs()
-        rhs.set(1.0)
-        ksp.solveTranspose(rhs, lhs)
-
-        sol = rhs.duplicate()
-        sol.set(0.0)
+        rhs.set(0.0)
         lindices = np.arange(0, misfit.size, dtype=PETSc.IntType)
-        sol.setValues(lindices, misfit, PETSc.InsertMode.ADD_VALUES)
-        sol.assemble()
-        sol *= lhs
+        rhs.setValues(lindices, misfit, PETSc.InsertMode.ADD_VALUES)
+        rhs.assemble()
+        ksp.solve(rhs, lhs)
 
-        toall, allvec = PETSc.Scatter.toAll(sol)
-        toall.scatter(sol, allvec, PETSc.InsertMode.INSERT)
+        toall, allvec = PETSc.Scatter.toAll(lhs)
+        toall.scatter(lhs, allvec, PETSc.InsertMode.INSERT)
 
         ksp.destroy()
         lhs.destroy()
-        sol.destroy()
         return allvec.array
 
 

tests/test_covariance.py
@@ -19,8 +19,7 @@ def objective_function_ad(x, x0, cov):
     """ adjoint model """
     misfit = np.array(x - x0)
     ones = np.ones_like(misfit)
-    res  = spsolve(cov.T, ones)
-    res *= misfit
+    res  = spsolve(cov, misfit)
     return res
 
 def gaussian_function(sigma, distance, length_scale):
@@ -38,7 +37,7 @@ def construct_covariance_matrix(sigma, coords, length_scale):
 
 
 size = 10
-x = np.ones(size)
+x = np.linspace(5, 10, 10)
 x0 = np.ones(size)*2.5
 sigma_x = np.ones(size)*10.0
 dx = x*0.1